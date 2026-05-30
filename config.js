e93d96e76e19359ba936effd9708f8d56e90efc9
Harshith Sharma P <85750824+HarshithSharma@users.noreply.github.com>
2026-05-30 22:59:35 +0530

    Change APPS_SCRIPT_URL to a placeholder
    Updated the Google Apps Script URL placeholder.

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // after deploying (see setup-guide.md for how)
 // =============================================
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzJB8ErG8Qur0OH0OtR-_d_zc1140BlXLp3wapM6XY5DeDJvbx1lpB18_zIwW33jmkq/exec";
+const APPS_SCRIPT_URL = ".......";
